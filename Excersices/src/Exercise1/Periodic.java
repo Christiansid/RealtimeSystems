ae50bc15c775cb4f227b668c46dbbba1c9547789
Christian Sidemo <Sidemo96@gmail.com>
2021-01-19 12:06:25 +0100

    Exercise1

---
+++ Excersices/src/Exercise1/Periodic.java
@@ -0,0 +1,6 @@
+package Exercise1;
+
+public class Periodic extends Thread {
+ private int period;
+	
+}
--- Labb1/src/Test.java
+++ Labb1/src/Test.java
@@ -4,6 +4,18 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(System.getProperty("java.runtime.version"));
+		Thread p1 = new Thread(()-> {
+			for(int p = 1; p<100; p++) {
+				System.out.println(p + " Hej");
+			}
+		});
+		Thread p2 = new Thread(()-> {
+			for(int i = 1; i < 100; i++) {
+				System.out.println(i + " Tjena");
+			}
+		});
+		p1.start();
+		p2.start();
 	}
 
 }
