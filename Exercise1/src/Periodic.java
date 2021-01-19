e8fbb81701c8c447c0e5c60ac93e2e489e8f5015
Christian Sidemo <Sidemo96@gmail.com>
2021-01-19 20:38:33 +0100

    Testing stuff out

--- Exercise1/src/Periodic.java
+++ Exercise1/src/Periodic.java
@@ -1,32 +1,45 @@
+import java.util.ArrayList;
 
 public class Periodic extends Thread {
-	private int period;
+	private final int period;
 
-	public Periodic(int period) {
-		this.period = period;
-		
-	}
-	@Override
-	public void run() {
-		try {
-		while(!Thread.interrupted()) {
-			System.out.print(period);
-			System.out.print(",");
+    public Periodic(final int period) {
+        this.period = period;
 
-				Thread.sleep(period);
-			} 
+    }
 
-		}catch (InterruptedException e) {
-			e.printStackTrace();
-			System.out.println("Thread interrupted");
-	}
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                System.out.print(period);
+                System.out.print(",");
 
-	}
-	
-	public static void main(String[] args) {
-		for(String arg : args) {
-			new Periodic(Integer.parseInt(arg)).start();
+                Thread.sleep(period);
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.out.println("Thread interrupted");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        if(args.length==0){
+			ArrayList<String> p = new ArrayList();
+			for(int i = 0; i<5;i++){
+				p.add(Integer.toString((i+1)*200));
+			}
+			for (String arg : p) {
+				new Periodic(Integer.parseInt(arg)).start();
+			}
+            }else{
+			for (String arg : args) {
+				new Periodic(Integer.parseInt(arg)).start();
+			}
 		}
+
 	}
 
 }
