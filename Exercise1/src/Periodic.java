25f87b0d75c96012fa0b4e08f1a1665eabe830fe
Christian Sidemo <Sidemo96@gmail.com>
2021-01-19 18:32:36 +0100

    Nothing working anymore giving up on JDK8 for vscode

--- Exercise1/src/Periodic.java
+++ Exercise1/src/Periodic.java
@@ -1,32 +1,9 @@
 
-public class Periodic extends Thread {
-	private int period;
+public class Periodic {
 
-	public Periodic(int period) {
-		this.period = period;
-		
-	}
-	@Override
-	public void run() {
-		try {
-		while(!Thread.interrupted()) {
-			System.out.print(period);
-			System.out.print(",");
-
-				Thread.sleep(period);
-			} 
-
-		}catch (InterruptedException e) {
-			e.printStackTrace();
-			System.out.println("Thread interrupted");
-	}
-
-	}
-	
 	public static void main(String[] args) {
-		for(String arg : args) {
-			new Periodic(Integer.parseInt(arg)).start();
-		}
+		// TODO Auto-generated method stub
+
 	}
 
 }
